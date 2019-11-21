e9bdad7b9233b4f2b9fe99af7e65990eaa4856e1
marcsous <17042435+marcsous@users.noreply.github.com>
2019-11-20 17:21:25 -0800

    Add files via upload

--- fft3.m
+++ fft3.m
@@ -3,5 +3,6 @@ function out = fft3(in,m,n,p)
 if nargin<2; m = size(in,1); end
 if nargin<3; n = size(in,2); end
 if nargin<4; p = size(in,3); end
-out = fft2(in,m,n);
+out = fft(in ,m,1);
+out = fft(out,n,2);
 out = fft(out,p,3);
--- ifft3.m
+++ ifft3.m
@@ -3,5 +3,6 @@ function out = ifft3(in,m,n,p,varargin)
 if nargin<2; m = size(in,1); end
 if nargin<3; n = size(in,2); end
 if nargin<4; p = size(in,3); end
-out = ifft2(in,m,n,varargin{:});
+out = ifft(in, m,1,varargin{:});
+out = ifft(out,n,2,varargin{:});
 out = ifft(out,p,3,varargin{:});
