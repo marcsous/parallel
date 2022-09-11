78bcd104e9a23a7912850e9c16c8c57175a6cbc4
Mark Bydder <17042435+marcsous@users.noreply.github.com>
2022-09-11 15:10:54 -0700

    Add files via upload

--- cconvn.m
+++ cconvn.m
@@ -14,18 +14,8 @@ for k = 1:numel(sA)
     end
 end
 
-% pad array for 'valid'
-switch numel(sA)
-    case 1; A = A(s{1});
-    case 2; A = A(s{1},s{2});
-    case 3; A = A(s{1},s{2},s{3});
-    case 4; A = A(s{1},s{2},s{3},s{4});
-    case 5; A = A(s{1},s{2},s{3},s{4},s{5});
-    case 6; A = A(s{1},s{2},s{3},s{4},s{5},s{6});
-    case 7; A = A(s{1},s{2},s{3},s{4},s{5},s{6},s{7});
-    case 8; A = A(s{1},s{2},s{3},s{4},s{5},s{6},s{7},s{8});       
-    otherwise; error('high dimension not supported - fix me');
-end
+% pad array
+A = A(s{:});
 
 % convn valid
 C = convn(A,B,'valid');
