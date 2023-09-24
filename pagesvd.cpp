fa88861db5ac5dbc4e530078ffbbdbb457073029
Mark Bydder <17042435+marcsous@users.noreply.github.com>
2023-09-24 07:18:20 -0700

    Add files via upload

--- pagesvd.cpp
+++ pagesvd.cpp
@@ -7,35 +7,33 @@
  *          [__] = pagesvd(X,'econ')
  *          [__] = pagesvd(__,outputForm)
  *
- *          outputForm can be 'matrix' or 'vector' [or 'trans' = same as 'matrix' but returns V' not V]
+ *          outputForm can be 'vector' or 'matrix' [or 'trans' to return V' not V]
  *
- * To compile:
- *          mex pagesvd.cpp -v -lmwlapack -R2018a
+ * Compile:
+ *          (MATLAB) mex pagesvd.cpp -lmwlapack -R2018a
+ *          (Octave) mex pagesvd.cpp -lopenblas -llapacke -R2018a
  */
-
 #include "mex.h"
-#include "lapack.h"
-#include <complex>
-#include <string.h>
 #include <omp.h>
+#include <cmath>
+#include <string>
 #include <algorithm>
-#include <cassert>
-#include <iostream>
 
+/* lapacke.h allows a custom complex type */
+#define lapack_complex_float  mxComplexSingle
+#define lapack_complex_double mxComplexDouble
+#include "lapacke.h"
+
+/* only works with interleaved complex */
 #if !MX_HAS_INTERLEAVED_COMPLEX
 #error "This MEX-file must be compiled with the -R2018a flag."
 #endif
 
-char* lower(char *buf);
+template <typename T>
+inline void transpose(T *A, int m, int n);
 
 template <typename T>
-inline void transpose(T *A, long m, long n);
-
-template <typename T>
-inline void conjugate(T *A, long m, long n);
-
-template <typename T>
-inline void shift_rows(T *S, long m, long n);
+inline void shiftrows(T *S, int m, int n);
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
@@ -43,175 +41,158 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     if (nrhs > 3) mexErrMsgTxt("Too many input arguments.");    
     if (nlhs > 3) mexErrMsgTxt("Too many output arguments.");          
     if (nrhs < 1) mexErrMsgTxt("Not enough input arguments.");
-    if (mxGetClassID(prhs[0])!=mxSINGLE_CLASS && mxGetClassID(prhs[0])!=mxDOUBLE_CLASS) mexErrMsgTxt("First argument must be numeric array.");
+    if (!mxIsDouble(prhs[0]) && !mxIsSingle(prhs[0])) mexErrMsgTxt("First argument must be numeric array.");
     
-    char jobz       = (nlhs > 1) ? 'A' : 'N'; // A(ll) or S(mall) [N(o U or V): singular values only
-    char outputForm = (nlhs > 1) ? 'M' : 'V'; // M(atrix) or V(ector) [T(rans): same as M but returns V' not V]
-    bool trans = false; // outputForm = 'T' returns the transpose V' instead of V (faster as lapack returns V')
-    
-    if (nrhs>1)
-    {
-        char buf[8];
-        if (mxGetString(prhs[1],buf,5)==0 && strncmp(lower(buf),"econ",5)==0)
-        {
-            if (nlhs > 1) jobz = 'S';
-        }
-        else if (mxGetString(prhs[1],buf,7)==0 && strncmp(lower(buf),"vector",7)==0)
-            outputForm = 'V';
-        else if (mxGetString(prhs[1],buf,7)==0 && strncmp(lower(buf),"matrix",7)==0)
-            outputForm = 'M';      
-        else if (mxGetString(prhs[1],buf,6)==0 && strncmp(lower(buf),"trans",6)==0)
-        {
-            trans = true;
-            outputForm = 'M';
-        }
-        else
-            mexErrMsgTxt("Second input argument must be 'econ', vector' or 'matrix'.");
+    char jobz       = (nlhs > 1) ? 'A' : 'N'; // A(ll), S(mall) or N(o U or V)
+    char outputForm = (nlhs > 1) ? 'M' : 'V'; // M(atrix), V(ector) or T(rans)
+    bool trans = false; // return transpose V' instead of V [outputForm = 'T']
+
+    for (int i = 1; i < nrhs; i++)
+    {   
+        char *c = mxArrayToString(prhs[i]); // no need to mxFree
+        std::string str = c ? c : "";
         
-        if (nrhs>2)
-        {
-            if (mxGetString(prhs[2],buf,7)==0 && strncmp(lower(buf),"vector",7)==0)
-                outputForm = 'V';
-            else if (mxGetString(prhs[2],buf,7)==0 && strncmp(lower(buf),"matrix",7)==0)
-                outputForm = 'M';       
-            else if (mxGetString(prhs[2],buf,6)==0 && strncmp(lower(buf),"trans",6)==0)
-            {
-                trans = true;
-                outputForm = 'M';
-            }
-            else
-                mexErrMsgTxt("Third input argument must be 'matrix' or 'vector'.");
-
-            if (jobz=='A') mexErrMsgTxt("Cannot specify 'matrix' and 'vector'.");
-        }
+        if (str.compare("econ")==0)
+            jobz = (nlhs > 1) ? 'S' : 'N';
+        else if (str.compare("vector")==0)
+            outputForm = 'V';
+        else if (str.compare("matrix")==0)
+            outputForm = 'M';      
+        else if (str.compare("trans")==0)
+            trans = outputForm = 'M';
+        else
+            mexErrMsgTxt("Arguments can be 'econ', 'vector' or 'matrix'.");
     }
-
-    /* duplicate input array (overwritten by lapack routines) */  
-    mxArray *a = mxDuplicateArray(prhs[0]);
-    if (!a) mexErrMsgTxt("Insufficent memory (a).");
-
-    /* check matrix */  
+    
+    /* input array: a */  
+    const mxArray *a = prhs[0];
+    const mwSize *adims = mxGetDimensions(a);    
     mwSize ndim = mxGetNumberOfDimensions(a);
-    const mwSize *adim = mxGetDimensions(a);
     
-    ptrdiff_t m = ndim>0 ? adim[0] : 1;
-    ptrdiff_t n = ndim>1 ? adim[1] : 1;
-    ptrdiff_t p = ndim>2 ? adim[2] : 1;
-    for (long i = 3; i < ndim; i++) p *= adim[i]; /* stack all higher dimensions */
+    mwSize m = ndim>0 ? adims[0] : 1;
+    mwSize n = ndim>1 ? adims[1] : 1;
+    mwSize p = ndim>2 ? adims[2] : 1;
+    for (int i = 3; i < ndim; i++) p *= adims[i]; // stack all higher dimensions
     
-    ptrdiff_t mn = std::min(m,n);
-    ptrdiff_t mx = std::max(m,n);
-
+    mwSize mn = std::min(m, n);
+    mwSize mx = std::max(m, n);
+    
     /* output arrays: s, u, v */   
-    ptrdiff_t sdim[3] = {m,n,p}; 
-    if (jobz=='S' || jobz=='N') sdim[0] = sdim[1] = mn;
-    if (outputForm=='V') sdim[1] = 1;
+    mwSize sdims[3] = {m,n,p}; 
+    if (jobz=='S' || jobz=='N') sdims[0] = sdims[1] = mn;
+    if (outputForm=='V') sdims[1] = 1;
 
-    ptrdiff_t udim[3] = {m,m,p};
-    if (jobz=='S') udim[1] = mn;
-    if (jobz=='N') udim[0] = udim[1] = 1;
+    mwSize udims[3] = {m,m,p};
+    if (jobz=='S') udims[1] = mn;
+    if (jobz=='N') udims[0] = udims[1] = 1;
 
-    ptrdiff_t vdim[3] = {n,n,p};
-    if (jobz=='S') vdim[0] = mn;
-    if (jobz=='N') vdim[0] = vdim[1] = 1;
+    mwSize vdims[3] = {n,n,p};
+    if (jobz=='S') vdims[0] = mn;
+    if (jobz=='N') vdims[0] = vdims[1] = 1;
+
+#if !MATLAB_MEX_FILE
+/* workaround for octave mxCreateNumericArray bug #64687 */
+#include "version.h"
+if (std::stod(OCTAVE_VERSION)<=8.3 && mxIsComplex(a)) udims[2] = vdims[2] = 2*p;
+#endif
     
-    mwSize xdim[std::max<mwSize>(ndim,3)]; /* for copying ptrdiff_t[] into mwSize[] */
-    mxArray *s = mxCreateNumericArray(3, std::copy_n(sdim,3,xdim)-3, mxGetClassID(a), mxREAL);    
-    mxArray *u = mxCreateNumericArray(3, std::copy_n(udim,3,xdim)-3, mxGetClassID(a), mxIsComplex(a) ? mxCOMPLEX : mxREAL);    
-    mxArray *v = mxCreateNumericArray(3, std::copy_n(vdim,3,xdim)-3, mxGetClassID(a), mxIsComplex(a) ? mxCOMPLEX : mxREAL);
-    
-    if (!s || !u || !v) mexErrMsgTxt("Insufficent memory (s, u, v).");
+    mxArray *s = mxCreateNumericArray(3, sdims, mxGetClassID(a), mxREAL);    
+    mxArray *u = mxCreateNumericArray(3, udims, mxGetClassID(a), mxIsComplex(a) ? mxCOMPLEX : mxREAL);    
+    mxArray *v = mxCreateNumericArray(3, vdims, mxGetClassID(a), mxIsComplex(a) ? mxCOMPLEX : mxREAL);
+    if (!s || !u || !v) mexErrMsgTxt("Insufficent memory (s, u, v, info).");
    
-    /* Get the number of threads from the Matlab engine (maxNumCompThreads) */
-    mxArray *matlabCallOut[1] = {0};
-    mxArray *matlabCallIn[1]  = {0};
-    mexCallMATLAB(1, matlabCallOut, 0, matlabCallIn, "maxNumCompThreads");
-    double *pthreads = mxGetPr(matlabCallOut[0]);
-    int nthreads = int(*pthreads);
-    if (nthreads == 1) mexWarnMsgTxt("pagesvd threads equals 1. Try increasing maxNumCompThreads().");
+    /* get no. threads from matlab (maxNumCompThreads) */
+    mxArray *mex_plhs[1], *mex_prhs[0];
+    mexCallMATLAB(1, mex_plhs, 0, mex_prhs, "maxNumCompThreads");
+    int nthreads = (int)mxGetScalar(mex_plhs[0]);
+    if (nthreads==1) mexWarnMsgTxt("pagesvd threads equals 1. Try increasing maxNumCompThreads.");
+
+    /* catch errors in omp block (mexErrMsgTxt crashes). on error, info stores bad matrix index */
+    volatile int info = 0;
+
     
-/* run in parallel on single cores */
+/* run in parallel on single threads */
 #pragma omp parallel num_threads(nthreads)
 if (m*n*p)
 { 
-    /* workspace calculations */
-    ptrdiff_t *iwork = (ptrdiff_t*)mxMalloc( 8 * mn * sizeof(ptrdiff_t) );
-      
-    ptrdiff_t sz = std::max(5*mn*mn+5*mn,2*mx*mn+2*mn*mn+mn);
-    void *rwork = (void*)mxMalloc( sz * mxGetElementSize(a) );
+    /* copy of i-th matrix of a (lapack overwrites) */
+    void *a_i = LAPACKE_malloc( m * n * mxGetElementSize(a) ); 
+    if (!a_i) info = -1; // cannot exit out of the omp block
     
-    ptrdiff_t lwork = std::max(mn*mn+2*mn+mx,4*mn*mn+6*mn+mx);
-    void *work = (void*)mxMalloc( lwork * mxGetElementSize(a) );
-    
-    if (!iwork || !rwork || !work) mexErrMsgTxt("Insufficent memory (work).");   
-    
-    /* svd and ctranspose v */   
-    #pragma omp for
-    for (long i = 0; i < p; i++)
-    {  
-        ptrdiff_t info;
-        
-        /* pointers to the i-th matrix (use char* to suppress compiler warnings */
-        void *a_i = (char*)mxGetData(a) + i * adim[0] * adim[1] * mxGetElementSize(a);
-        void *s_i = (char*)mxGetData(s) + i * sdim[0] * sdim[1] * mxGetElementSize(s);
-        void *u_i = (char*)mxGetData(u) + i * udim[0] * udim[1] * mxGetElementSize(u);
-        void *v_i = (char*)mxGetData(v) + i * vdim[0] * vdim[1] * mxGetElementSize(v);
+    /* svd and transpose v */   
+    #pragma omp for schedule(static,1)
+    for (int i = 0; i < p; i++)
+    {
+        /* point to the i-th matrix (use char* for bytes) */
+        void *s_i = (char*)mxGetData(s) + i * sdims[0] * sdims[1] * mxGetElementSize(s);
+        void *u_i = (char*)mxGetData(u) + i * udims[0] * udims[1] * mxGetElementSize(u);
+        void *v_i = (char*)mxGetData(v) + i * vdims[0] * vdims[1] * mxGetElementSize(v);
 
-        // real float
-        if(!mxIsComplex(a) && !mxIsDouble(a))
+        /* on error skip */
+        if (info != 0)
+            ;
+        /* real float */
+        else if(!mxIsComplex(a) && !mxIsDouble(a))
         {
-            sgesdd(&jobz, &m, &n, (float*)a_i, &m, (float*)s_i, (float*)u_i, udim, (float*)v_i, vdim, (float*)work, &lwork, iwork, &info);
-            
-            if (trans==false) transpose((float*)v_i, vdim[0], vdim[1]);
-            if (outputForm=='M') shift_rows((float*)s_i, sdim[0], sdim[1]);
+            std::copy_n((float*)mxGetData(a) + i * m * n, m * n, (float*)a_i);
+            if (LAPACKE_sgesdd(LAPACK_COL_MAJOR, jobz, m, n, (float*)a_i, adims[0], (float*)s_i, (float*)u_i, udims[0], (float*)v_i, vdims[0])) info = i+1;
+            if (trans==false) transpose((float*)v_i, vdims[0], vdims[1]);
+            if (outputForm=='M') shiftrows((float*)s_i, sdims[0], sdims[1]);
         }
-        
-        // real double
+        /* real double */
         else if(!mxIsComplex(a) &&  mxIsDouble(a))
         {
-            dgesdd(&jobz, &m, &n, (double*)a_i, &m, (double*)s_i, (double*)u_i, udim, (double*)v_i, vdim, (double*)work, &lwork, iwork, &info);
-        
-            if (trans==false) transpose((double*)v_i, vdim[0], vdim[1]);
-            if (outputForm=='M') shift_rows((double*)s_i, sdim[0], sdim[1]);
-
+            std::copy_n((double*)mxGetData(a) + i * m * n, m * n, (double*)a_i);   
+            if (LAPACKE_dgesdd(LAPACK_COL_MAJOR, jobz, m, n, (double*)a_i, adims[0], (double*)s_i, (double*)u_i, udims[0], (double*)v_i, vdims[0])) info = i+1;
+            if (trans==false) transpose((double*)v_i, vdims[0], vdims[1]);
+            if (outputForm=='M') shiftrows((double*)s_i, sdims[0], sdims[1]);
         }   
-        
-        // complex float
+        /* complex float */
         else if( mxIsComplex(a) && !mxIsDouble(a))
         {
-            cgesdd(&jobz, &m, &n, (float*)a_i, &m, (float*)s_i, (float*)u_i, udim, (float*)v_i, vdim, (float*)work, &lwork, (float*)rwork, iwork, &info);
-
-            if (trans==false) transpose((std::complex<float>*)v_i, vdim[0], vdim[1]);
-            if (trans==false) conjugate((std::complex<float>*)v_i, vdim[0], vdim[1]);   
-            if (outputForm=='M') shift_rows((float*)s_i, sdim[0], sdim[1]);      
+            std::copy_n((mxComplexSingle*)mxGetData(a) + i * m * n, m * n, (mxComplexSingle*)a_i);   
+            if (LAPACKE_cgesdd(LAPACK_COL_MAJOR, jobz, m, n, (mxComplexSingle*)a_i, adims[0], (float*)s_i, (mxComplexSingle*)u_i, udims[0], (mxComplexSingle*)v_i, vdims[0])) info = i+1;
+            if (trans==false) transpose((mxComplexSingle*)v_i, vdims[0], vdims[1]);
+            if (outputForm=='M') shiftrows((float*)s_i, sdims[0], sdims[1]);      
         }
-        
-        // complex double
+        /* complex double */
         else if( mxIsComplex(a) &&  mxIsDouble(a))
         {
-            zgesdd(&jobz, &m, &n, (double*)a_i, &m, (double*)s_i, (double*)u_i, udim, (double*)v_i, vdim, (double*)work, &lwork, (double*)rwork, iwork, &info);
-            
-            if (trans==false) transpose((std::complex<double>*)v_i, vdim[0], vdim[1]);
-            if (trans==false) conjugate((std::complex<double>*)v_i, vdim[0], vdim[1]);
-            if (outputForm=='M') shift_rows((double*)s_i, sdim[0], sdim[1]);
+            std::copy_n((mxComplexDouble*)mxGetData(a) + i * m * n, m * n, (mxComplexDouble*)a_i);   
+            if (LAPACKE_zgesdd(LAPACK_COL_MAJOR, jobz, m, n, (mxComplexDouble*)a_i, adims[0], (double*)s_i, (mxComplexDouble*)u_i, udims[0], (mxComplexDouble*)v_i, vdims[0])) info = i+1;
+            if (trans==false) transpose((mxComplexDouble*)v_i, vdims[0], vdims[1]);
+            if (outputForm=='M') shiftrows((double*)s_i, sdims[0], sdims[1]);
         }
+      
+        /* lapack doesn't catch Inf but returns NaN singular values */
+        if (mxIsDouble(a) ? std::isnan(*(double*)s_i) : std::isnan(*(float*)s_i)) info = i+1; 
         
-        if(info) mexErrMsgTxt("dgesdd failed (run).");
-    
     } /* end of pragma omp for loop */
-    
-    mxFree(work);
-    mxFree(iwork);
-    mxFree(rwork);
+
+    if (a_i) LAPACKE_free(a_i);
     
 } /* end of pragma omp parallel block */
 
+
+    /* now can throw error */
+    if (info)
+    {
+        std::string str = "LAPACKE_ gesdd() failed ";
+        if(!mxIsComplex(a) && !mxIsDouble(a)) str.replace(8,1,"s");
+        if(!mxIsComplex(a) &&  mxIsDouble(a)) str.replace(8,1,"d");
+        if( mxIsComplex(a) && !mxIsDouble(a)) str.replace(8,1,"c");
+        if( mxIsComplex(a) &&  mxIsDouble(a)) str.replace(8,1,"z");
+        if (info == -1) str += "due to insufficient memory (a_i).";
+        else str += "at matrix " + std::to_string(info) + ".";
+        mexErrMsgTxt(str.c_str());
+    }
+    
     /* reshape to match input */
-    std::copy_n(adim, ndim, xdim);
-    if (trans==false) std::swap(vdim[0], vdim[1]);
-    xdim[0] = sdim[0]; xdim[1] = sdim[1]; mxSetDimensions(s, xdim, ndim);    
-    xdim[0] = udim[0]; xdim[1] = udim[1]; mxSetDimensions(u, xdim, ndim);
-    xdim[0] = vdim[0]; xdim[1] = vdim[1]; mxSetDimensions(v, xdim, ndim);
+    mwSize xdims[ndim];
+    std::copy_n(adims, ndim, xdims);
+    xdims[0] = sdims[0]; xdims[1] = sdims[1]; mxSetDimensions(s, xdims, ndim);    
+    xdims[0] = udims[0]; xdims[1] = udims[1]; mxSetDimensions(u, xdims, ndim);
+    xdims[0] = vdims[trans]; xdims[1] = vdims[!trans]; mxSetDimensions(v, xdims, ndim);
 
     if (nlhs > 2)
     {
@@ -231,48 +212,40 @@ if (m*n*p)
         mxDestroyArray(u);
         mxDestroyArray(v);
     }
-    
-    mxDestroyArray(a);
 }
       
 
-// In-place convert to lowercase
-char* lower(char *buf)
+// In-place matrix shift
+template <typename T>
+inline void shiftrows(T *S, int m, int n)
 {
-    for (long i = 0; buf[i]; i++)
-        buf[i] = std::tolower(buf[i]);
-    
-    return buf;
+    for (int i = 1; i < std::min(m, n); i++)
+        std::swap(S[i], S[i+m*i]);
 }
 
 
 // In-place complex conjugate
 template<typename T>
-inline void conjugate(T *A, long m, long n)
+inline void conjugate(T *A, int m, int n)
 {
-    for (long i = 0; i < m*m; i++)
-        A[i].imag(-A[i].imag());
+    for (int i = 0; i < m*n; i++)
+        A[i].imag = -A[i].imag;
 }
-
-
-// In-place matrix shift
-template <typename T>
-inline void shift_rows(T *S, long m, long n)
-{
-    for (long i = 1; i < std::min(m,n); i++)
-        std::swap(S[i], S[i+m*i]);
-}
+template<> void conjugate(float *, int, int) {}
+template<> void conjugate(double*, int, int) {}
 
 
 // In-place matrix transpose
 template<typename T>
-inline void transpose(T *A, long m, long n)
+inline void transpose(T *A, int m, int n)
 {
+    conjugate(A, m, n);
+    
     if (m < n) std::swap(m, n);
 
     while (m > 1 && n > 1)
     {
-        for (long i = 1; i < m; i++)
+        for (int i = 1; i < m; i++)
             std::rotate(A+i, A+i*n, A+i*n+1);
         
         A += m;
