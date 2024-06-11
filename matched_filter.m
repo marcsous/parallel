3c05a03b32741834b91f091c4f542daa438066e0
Mark Bydder <17042435+marcsous@users.noreply.github.com>
2024-06-11 07:13:08 -0700

    Add files via upload

--- matched_filter.m
+++ matched_filter.m
@@ -1,36 +1,42 @@
 function [im coils noise] = matched_filter(data,np)
-% Matched filter coil combination.
-% Ref: David Walsh MRM 2000;43:682
+% Matched filter coil combination (Walsh MRM 2000;43:682)
 %
 % Inputs:
 %  data = complex images [nx ny nz nc (ne)] 
-%  np = no. pixels in the neighborhood (200)
+%  np = no. pixels in the neighborhood (100)
 %
 % Output:
 %  im = the combined image [nx ny nz (ne)] 
 %  coils = the optimal coil filters 
-%  noise = noise std estimate
-
-% run example
-if nargin==0
-    load head
-    [nx ny nc] = size(data); % 2D
-    data = reshape(data,nx,ny,1,nc);
-end
+%  noise = noise std estimate (maybe?)
 
 %% parse inputs
-if ndims(data)<4
-    error('data size must be [nx ny nz nc]');
-end
 [nx ny nz nc ne] = size(data);
-dim = 4; % coil dimension
 
-% np=200 is 90% optimal but most benefit comes <50
+% try to accomodate 2D, 3D and a time dimension (echo)
+if isempty(data)
+    error('data cannot be empty');
+elseif ndims(data)==3 
+    nc = nz; nz = 1;
+elseif ndims(data)<4 || ndims(data)>5
+    error('data must be [nx ny nz nc ne]');
+end
+
+% coil dimension (must be 4)
+dim = 4;
+data = reshape(data,[nx ny nz nc ne]);
+
+% np=200 is 90% optimal but most benefit comes earlier
 if ~exist('np','var')
-    np = 49;
+    np = 100;
 else
     np = max(nc,np); % lower limit
 end 
+
+% warn about silliness
+if np*ne > 1000
+    warning('effective neighborhood size (ne*np=%i) is excessively large',np*ne);
+end
 
 %% neighborhood of np nearest pixels
 
@@ -43,14 +49,19 @@ L(1) = (np/L(3))^(1/2);
                  -ceil(L(2)/2):ceil(L(2)/2), ...
                   -fix(L(3)/2):fix(L(3)/2));
 
-% sort by distance
-d = sqrt(x.^2 + y.^2 + z.^2);
-[d k] = sort(reshape(d,[],1));
+% sort by radius
+r = sqrt(x.^2 + y.^2 + z.^2);
+[r k] = sort(reshape(r,[],1));
 
-% keep nearest np
-k = k(1:np);
-d = d(1:np);
+% round to nearest symmetric kernel
+ok = find(diff(r));
+[~,j] = min(abs(np-ok));
+np = ok(j); k = k(1:np);
+
+% keep nearest np points
 x = x(k); y = y(k); z = z(k);
+
+disp([mfilename ': [' num2str(size(data)) '] np=' num2str(np) ' r=' num2str(r(np),'%f')]);
 
 %% convolution matrix
 coils = zeros(nx,ny,nz,nc,ne,np,'like',data);
@@ -75,7 +86,7 @@ coils = coils(:,1,:,:,:);
 % reorder for dot: nx ny nz nc 1
 coils = permute(coils,[3 4 5 1 2]);
 
-% noise std estimate
+% noise std estimate?
 noise = mean(reshape(noise(2:end,:,:,:,:),[],1)) / sqrt(ne*np);
 
 %% final image
@@ -83,4 +94,3 @@ im = sum(coils.*data,dim);
 
 % collape coil dimension
 im = reshape(im,[nx ny nz ne]);
-
